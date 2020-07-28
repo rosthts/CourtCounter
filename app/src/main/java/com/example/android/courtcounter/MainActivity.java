126bb7fe835ebf01508c4ae593538c4fb0933ba1
rosthts <rosthts@gmail.com>
2020-07-28 14:02:02 +0300

    First

---
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -0,0 +1,73 @@
+package com.example.android.courtcounter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    int scoreTeamA = 0;
+    int scoreTeamB = 0;
+
+    /**
+     * Displays the given score for Team A.
+     */
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void displayForTeamA(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    public void addThreeForTeamA(View v){
+        scoreTeamA = scoreTeamA +3;
+        displayForTeamA(scoreTeamA);
+    }
+
+    public void addTwoForTeamA(View v){
+        scoreTeamA = scoreTeamA +2;
+        displayForTeamA(scoreTeamA);
+    }
+
+    public void addOneForTeamA(View v){
+        scoreTeamA = scoreTeamA +1;
+        displayForTeamA(scoreTeamA);
+    }
+
+    public void displayForTeamB(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+
+    public void addThreeForTeamB(View v){
+        scoreTeamB = scoreTeamB +3;
+        displayForTeamB(scoreTeamB);
+    }
+
+    public void addTwoForTeamB(View v){
+        scoreTeamB = scoreTeamB +2;
+        displayForTeamB(scoreTeamB);
+    }
+
+    public void addOneForTeamB(View v){
+        scoreTeamB = scoreTeamB +1;
+        displayForTeamB(scoreTeamB);
+    }
+
+    public void reset (View v){
+        scoreTeamA = 0;
+        scoreTeamB = 0;
+        displayForTeamB(scoreTeamB);
+        displayForTeamA(scoreTeamA);
+
+    }
+
+}
